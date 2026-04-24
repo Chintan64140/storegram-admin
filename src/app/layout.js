d11ca47ec498d3d71aec73b793f6b50032d882c4
Chintan64140 <chintan6414@gmail.com>
2026-04-24 15:24:33 +0530

    p

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,7 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import LayoutWrapper from "@/components/LayoutWrapper";
 import AuthGuard from "@/components/AuthGuard";
 import { Suspense } from "react";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "StoreGram Admin",
@@ -14,9 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} min-h-screen bg-background text-foreground antialiased`}
-      >
+      <body className="min-h-screen bg-background text-foreground antialiased">
         <AuthGuard>
           <LayoutWrapper>
             <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
